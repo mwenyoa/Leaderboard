faeebf765c369e3dc2a637f895760380e3579024
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-05-12 20:41:10 +0200

    Add logic to select dom elements add event listners  and call leaderboard methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import LeaderBoard from './leaderScores.js';
+
+const leaderB = new LeaderBoard();
+leaderB.displayData();
+// refresh btn
+document.querySelector('.refresh').addEventListener('click', () => {
+  leaderB.displayData();
+});
+// submit data
+const scoreForm = document.querySelector('.add-score');
+scoreForm.addEventListener('submit', (ex) => {
+  ex.preventDefault();
+  const username = scoreForm.elements.name.value;
+  const score = scoreForm.elements.score.value;
+  leaderB.postData(username, score).then((res) => {
+    if (!res.error) {
+      scoreForm.reset();
+    }
+  });
+});
