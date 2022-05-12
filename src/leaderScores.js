a7c164f838ced0dc7bb2cf5d92cdc68c2f0ba02e
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-05-12 20:26:22 +0200

    Add method to retrieve scores

--- src/leaderScores.js
+++ src/leaderScores.js
@@ -4,4 +4,13 @@ export default class LeaderBoard {
     this.id = '46TANuqp4TocEYMLV1Ht';
     this.participators = [];
   }
+
+  // retrieve participant scores
+  async retrieveScores() {
+    const response = await fetch(`${this.url + this.id}/scores/`)
+      .then((res) => res.json())
+      .then((data) => ({ error: false, participators: data.result }))
+      .catch(() => ({ error: true, participators: this.participators }));
+    return response;
+  }
 }
