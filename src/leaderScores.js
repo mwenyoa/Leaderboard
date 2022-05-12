297c74eb2968f33e04a96547d0ea65d3e2d57d8f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-05-12 20:23:26 +0200

    Add Leaderboard class and its constructor method

---
+++ src/leaderScores.js
@@ -0,0 +1,7 @@
+export default class LeaderBoard {
+  constructor() {
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+    this.id = '46TANuqp4TocEYMLV1Ht';
+    this.participators = [];
+  }
+}
